dea0d55891a5c282cb8194c718e688210daa4380
muhammad-hamza <Muh-Hamza-99>
2022-07-04 19:32:38 +0300

    Basic user schema and model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, "Please tell us your name!"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please tell us your email!"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Email is of an incorrect format!"],
+    },
+    role: {
+        type: String,
+        enum: ["user", "supplier", "admin"],
+        default: "user", 
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password!"],
+        minlength: 8,
+        select: false,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please confirm your password!"],
+        validate: {
+            validator: function(value) {
+                return value === this.password;
+            },
+            message: "Passwords do not match!",
+        },
+    },
+    active: {
+        type: Boolean,
+        default: true,
+        select: false,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
