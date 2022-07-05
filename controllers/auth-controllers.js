103727ad2c8c6e491fcff1afe03ac26f4ed592ea
muhammad-hamza <Muh-Hamza-99>
2022-07-05 19:22:02 +0300

    Login controller

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -5,3 +5,10 @@ const register = async (req, res, next) => {
     const user = await User.create({ name, email, password, passwordConfirm });
     res.status(201).json({ status: "success", data: { user } })
 };
+
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (!user || !(user.correctPassword(password, user.password))) return;
+    
+};
