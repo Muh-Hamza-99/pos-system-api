6aa0449724cb24ba71ed6523fc660bc141f278c9
muhammad-hamza <Muh-Hamza-99>
2022-07-05 23:25:47 +0300

    Updating field in schema and controllers

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: {
+    username: {
         type: String, 
         required: [true, "Please tell us your name!"],
     },
