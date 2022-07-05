cc4b4dcf5b05fa770e5982d4120c81d4c2e5b9fb
muhammad-hamza <Muh-Hamza-99>
2022-07-05 23:33:34 +0300

    Helper functions to send back json web tokens

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,8 +1,24 @@
+const JWT = require("jsonwebtoken");
+
 const User = require("../models/User");
+
+const signToken = id => JWT.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TOKEN_EXPIRES_IN });
+
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+    };
+    res.cookie("jwt", token, cookieOptions);
+    user.password = undefined;
+    res.status(statusCode).json({ status: "success", token });
+};
 
 const register = async (req, res, next) => {
     const { username, email, password, passwordConfirm } = req.body;
-    const user = await User.create({ name, email, password, passwordConfirm });
+    const user = await User.create({ username, email, password, passwordConfirm });
     res.status(201).json({ status: "success", data: { user } })
 };
 
