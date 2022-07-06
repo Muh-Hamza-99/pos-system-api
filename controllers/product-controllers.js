06c4f2a1a3afd6446e3a394fd055c9aa945cc985
muhammad-hamza <Muh-Hamza-99>
2022-07-06 03:33:40 +0300

    CRUD operation controllers for product resource

---
+++ controllers/product-controllers.js
@@ -0,0 +1,34 @@
+const Product = require("../models/Product");
+
+const AppError = require("../utilities/app-error");
+
+const getAllProducts = async (req, res, next) => {
+    const products = await Product.find({});
+    res.status(200).json({ status: "success", results: products.length, data: { products }});
+};
+
+const getOneProduct = async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    if (!product) return next(new AppError("No product with the provided ID!", 404));
+    res.status(200).json({ status: "success", data: { product }});
+};
+
+const createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({ status: "success", data: { product } });
+};
+
+const updateProduct = async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+    if (!product) return next(new AppError("No product with the provided ID!", 404));
+    res.status(200).json({ status: "success", data: { product } });
+};
+
+const deleteProduct = async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id); 
+    if (!product) return next(new AppError("No product with the provided ID!", 404));
+    res.status(204).json({ status: "success", data: null });
+};
