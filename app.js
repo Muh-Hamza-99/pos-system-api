277eadd82e3c4005d12b5cdaef9539106057af52
muhammad-hamza <Muh-Hamza-99>
2022-07-06 03:11:28 +0300

    404 error handler

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ const app = express();
 
 const userRouter = require("./routes/user-routes");
 
+const AppError = require("./utilities/app-error");
 const globalErrorHandler = require("./middleware/error-handler");
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+    res.status(404).json({ status: "fail", message: `Can't find ${req.originalUrl} on this server!` });
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 app.use(globalErrorHandler);
 
