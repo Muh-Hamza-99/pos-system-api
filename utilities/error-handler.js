c191bdc763484450611b41ebb90387797949b353
muhammad-hamza <Muh-Hamza-99>
2022-07-06 03:07:28 +0300

    Sending errors differently based on the environment

--- utilities/error-handler.js
+++ utilities/error-handler.js
@@ -1,3 +1,26 @@
+const AppError = require("./app-error");
+
+const sendErrorDevelopment = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        error,
+        message: error.message,
+        stack: error.stack,
+    });
+};
+
+const sendErrorProduction = (error, res) => {
+    if (error.isOperational) {
+        res.status(error.statusCode).json({ 
+            status: error.status, 
+            message: error.message, 
+        });
+    } else {
+        console.log(`Oh no, something went wrong! ${error}`);
+        res.status(500).json({ status: "error", message: "Something went very wrong!" });
+    };
+};
+
 const globalErrorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
