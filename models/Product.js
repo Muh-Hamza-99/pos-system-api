ee3867b5c4f779bd5b5666edca8bf0f25cc772e4
muhammad-hamza <Muh-Hamza-99>
2022-07-08 18:59:34 +0300

    Updating Product schema

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Please tell the product name!"],
+        unique: true,
     },
     description: {
         type: String,
