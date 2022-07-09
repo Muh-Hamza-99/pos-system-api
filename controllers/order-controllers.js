28141bf8e783be9cf068f56352da7974e01ae13c
muhammad-hamza <Muh-Hamza-99>
2022-07-09 03:17:08 +0300

    Allowing buyer to both add and remove products and associated quantities from order

--- controllers/order-controllers.js
+++ controllers/order-controllers.js
@@ -21,12 +21,15 @@ const createOrder = catchAsync(async (req, res, next) => {
 });
 
 const updateOrder = catchAsync(async (req, res, next) => {
+    const { method } = req.query;
+    if (method !== "add" && method !== "remove") return next(new AppError("Missing/Invalid query parameter; it can only be 'add' or 'remove'!", 400));
     const { quantity } = req.body;
     const { productID, orderID } = req.params;
     let order = await Order.findById(orderID);
     if (!order) return next(new AppError("No order with the provided ID!", 404));
-    if (order.productAlreadyInOrder(productID)) return next(new AppError("Order already contains that product!", 406));
-    order = await Order.findByIdAndUpdate(orderID, { $push: { "products": { product: productID, quantity } } }, { new: true, runValidators: true });
+    const productAlreadyInOrder = order.productAlreadyInOrder(productID);
+    if (method === "add" && !productAlreadyInOrder) order = await Order.findByIdAndUpdate(orderID, { $push: { "products": { product: productID, quantity } } }, { new: true, runValidators: true });
+    if (method === "remove" && productAlreadyInOrder) order = await Order.findByIdAndUpdate(orderID, { $pull: { "products": { product: productID } } }, { new: true, runValidators: true });
     res.status(200).json({ status: "success", data: { order } });
 });
 
