95432cc4774a746ba7a1401e4c2d9aced0af4741
muhammad-hamza <Muh-Hamza-99>
2022-07-11 20:56:35 +0300

    Setting up CRUD user routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,7 +6,26 @@ const {
     login,
 } = require("../controllers/auth-controllers");
 
+const {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require("../controllers/user-controllers");
+
 router.post("/register", register);
 router.post("/login", login);
+
+router
+    .route("/")
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route("/:id")
+    .get(getOneUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
