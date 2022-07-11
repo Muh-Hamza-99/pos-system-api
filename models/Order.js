ad98afc6a8776428c4a862c8e1386ef2ec960653
muhammad-hamza <Muh-Hamza-99>
2022-07-11 21:22:44 +0300

    Finishing two-way reference between orders and user

--- models/Order.js
+++ models/Order.js
@@ -24,6 +24,10 @@ const orderSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
 });
 
 orderSchema.methods.productAlreadyInOrder = function(productID) {
