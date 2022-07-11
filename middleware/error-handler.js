3077ff2ed4a767c60483417bb9ea896f8c4c8349
muhammad-hamza <Muh-Hamza-99>
2022-07-11 21:19:54 +0300

    Handling special errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -25,7 +25,16 @@ const globalErrorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
     if (process.env.NODE_ENV === "development") sendErrorDevelopment(err, res);
-    else if (process.env.NODE_ENV === "production") sendErrorProduction(err, res); 
+    else if (process.env.NODE_ENV === "production") {
+        let error = { ...err };
+        if (error.name === "CastError") error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldDB(error);
+        if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+        if (error.name === "JsonWebTokenError") error = handleJWTError();
+        if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+        sendErrorProduction(err, res);
+    };
+    next();
 };
 
 module.exports = globalErrorHandler;
